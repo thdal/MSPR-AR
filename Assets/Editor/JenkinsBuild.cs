42cc6ee6d3fa43e9d0dc394e4d7aa867ebc1f188
thdal <36514056+thdal@users.noreply.github.com>
2022-05-21 14:31:29 +0200

    Update JenkinsBuild.cs

--- Assets/Editor/JenkinsBuild.cs
+++ Assets/Editor/JenkinsBuild.cs
@@ -28,6 +28,7 @@ public class JenkinsBuild {
     //   +2: VRDungeons
     //   +3: /Users/Shared/Jenkins/Home/jobs/VRDungeons/builds/47/output
     string[] args = System.Environment.GetCommandLineArgs();
+    args[args.Length] += "/output"
     for (int i=0; i<args.Length; i++){
           System.Console.WriteLine("argument n°" + i + " : "  + args[i]);
 
@@ -35,11 +36,11 @@ public class JenkinsBuild {
 
     for (int i=0; i<args.Length; i++){
       if (args[i] == "-executeMethod"){
-        if (i+3 < args.Length){
+        if (i+4 < args.Length){
           // BuildMacOS method is args[i+1]
-          appName = args[i+1];
-          targetDir = args[i+2];
-          i += 2;
+          appName = args[i+2];
+          targetDir = args[i+3];
+          i += 3;
         }
         else {
           System.Console.WriteLine("[JenkinsBuild] Incorrect Parameters for -executeMethod Format: -executeMethod BuildMacOS <app name> <output dir>");
